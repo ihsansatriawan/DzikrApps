b1a2b688427ababcc42b5323bf06f31167007e4b
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2017-09-24 06:37:55 +0700

    Integrate with router flux

---
+++ App/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import RootScreens from './Screens/RootScreens'
+
+export default class App extends Component {
+  render() {
+    return (
+      <RootScreens />
+    )
+  }
+}
