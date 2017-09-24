a2a6de1d954d82fcc7cb3fac0ea4bc60107a2d56
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2017-09-24 21:03:55 +0700

    Use Drawer Navigation

--- App/Config/router.js
+++ App/Config/router.js
@@ -1,20 +1,38 @@
 import React from 'react';
-import { StackNavigator } from 'react-navigation';
+import { Image } from 'react-native'
+import { StackNavigator, DrawerNavigator } from 'react-navigation';
 
 import HomeScreen from '../Screens/Home'
 import AboutScreen from '../Screens/About'
+import BaseStyle from '../Style/Base'
 
-const Root = StackNavigator({
+const Root = DrawerNavigator({
   Home: {
     screen: HomeScreen,
     navigationOptions: {
       title: 'Dzikr App',
+      drawer: {
+        icon: () => (
+          <Image 
+            source={require('../Assets/imgs/home.png')}
+            style={[BaseStyle.tabIcon, {tintColor: 'black'}]}
+          />
+        )
+      }
     },
   },
   About: {
     screen: AboutScreen,
     navigationOptions: {
       title: 'About',
+      drawer: {
+        icon: () => (
+          <Image 
+            source={require('../Assets/imgs/home.png')}
+            style={[BaseStyle.tabIcon, {tintColor: 'black'}]}
+          />
+        )
+      }
     },
   },
 })
